6f74a88e55ef66cd6ea815731cba879fcb28c6d6
Rodrigo Laguna <rodrigolaguna90@gmail.com>
2021-04-10 06:29:48 -0500

    primer commit

---
+++ LecturaDeArchivos/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace LecturaDeArchivos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.IO.StreamReader archivo = null;
+
+            try
+            {
+                string fila;
+                int contador = 0;
+                string path = @"C:\Users\Rodrigo\Desktop\prueba.txt";
+
+                archivo = new System.IO.StreamReader(path);
+
+                while ( (fila = archivo.ReadLine()) != null )
+                {
+                    Console.WriteLine(fila);
+                    contador++;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error en la lectura del archivo");
+            }
+            finally
+            {
+                if ( archivo != null)
+                {
+                    archivo.Close();
+                    Console.WriteLine("Conexión cerrada");
+                }
+            }
+        }
+    }
+}
